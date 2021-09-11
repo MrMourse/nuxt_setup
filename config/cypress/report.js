d1a2112af4b491d0ea1d76b45dd4517bb0142cfa
Mourse <moursemotte@gmail.com>
2021-09-11 20:02:49 +0200

    feat(config): add cypress test end to end

---
+++ config/cypress/report.js
@@ -0,0 +1,13 @@
+const report = require('multiple-cucumber-html-reporter')
+report.generate({
+    jsonDir: './reports/cucumber-json',
+    reportPath: './reports/report.html',
+    metadata: {
+        browser: { name: 'electron', version: '79' },
+        device: 'Local test machine',
+        platform: {
+            name: 'mac',
+            version: 'Catalina'
+        }
+    }
+})
